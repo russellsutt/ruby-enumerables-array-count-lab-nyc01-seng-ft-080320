f67bc68f29cbeb0d4385ff06254cdcb086ebfc69
Russell Sutter <russell.sutter@gmail.com>
2020-07-13 19:21:42 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -3,15 +3,13 @@ def count_strings(array)
   
   count = 0
   new_array = []
-  index = 0
   
-  while array.count < count do array.each |string| 
+  while array.count < count do array.each |string|
       if string.class == String
       new_array << string
       end
     count += 1
   end
-  index += 1
   return new_array.count
 end
 
